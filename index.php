65aacace8dd3cc8040ef2c036a22bae3aeacc639
2613df <2613df@163.com>
2019-01-23 16:55:42 +0800

    version 2.2

--- index.php
+++ index.php
@@ -1,134 +1,162 @@
 <?php
 $visit=true;
+$readOnly=false;
 include_once "./lib/config.php";
-$path = $FOLDER."/".$name;
-$file_head = file_get_contents($path,FALSE,NULL,0,47);
-include "./content/templates/default/header.php";
-?>
-<div class="txa">
-<?php
-$isLocked=substr($file_head,0,15) == $pw_tag ? 1:0;
-$isEncrypted=$isLocked;
-if (isset($_POST["checkpw"])) {
+include_once "./lib/do.php";
+include $templateFolder."header.php";
+include $templateFolder."show.php";
+
+
+
+//if(!$isEncrypted){
+$_SESSION[dataEncrypt($name)]=(!$isEncrypted)?(1):($_SESSION[dataEncrypt($name)]);
+//}
+
+
+
+if (isset($_POST["checkpw"])) {//检查密码正确性
 	$do=1;
-	if (md5($_POST["submit_pw"]) == substr($file_head,15,32) ){
-	$isLocked=0;
-	$do=0;
-	}else {
-	echo '<div class="alert alert-danger" role="alert"><strong>密码错误，将在3s后重试</strong>密码遗失不可找回</div><meta http-equiv="refresh" content="3; url=/'.$_GET[f].'">';
-	$needBtn=0;
-	}
-}
-if (isset($_POST["setpw"])) {
-		$do=1;
-		if(!$isEncrypted){
-			echo "<br><br><br><br><br>
-					<form action='/$_GET[f]' method='post' class='form-inline obody'>
-						<div class='form-group'>
-							<p class='form-control-static'>请设定密码：</p>
-							<input type='password' class='form-control' name='submit_pw' />
-						</div>
-						<input type='submit' class='btn btn-default' value='确认' />
-						<a href='javascript:void(0)' class='btn btn-default' onclick='history.go(-1)'>算了</a>
-						<input type='hidden' name='setpw2' value='1' />	";
-	
-			//exit();
-			$needBtn=0;
-		}else{
-					echo "<br><br><br><br><br>
-					<form action='/$_GET[f]' method='post' class='form-inline obody'>
-						<div class='form-group'>
-							<span class='form-control-static'>旧密码：</span>
-							<input type='password' class='form-control' name='submit_pw_old' />
-							<span class='form-control-static'>请设定密码：</span>
-							<input type='password' class='form-control' name='submit_pw' />
-						</div>
-						<input type='submit' class='btn btn-default' value='确认' />
-						<a href='javascript:void(0)' class='btn btn-default' onclick='history.go(-1)'>算了</a>
-						<input type='hidden' name='setpw2' value='1' />	";
-		}
-}
-if (isset($_POST["setpw2"])) {
-		$do=1;
-	if(!$isEncrypted){
-		if (substr($file_head,0,15) == $pw_tag ){
-			$new_text = $pw_tag.md5($_POST['submit_pw']).file_get_contents($path,FALSE,NULL,47);
-			file_put_contents($path, $new_text);
-			}else {
-				$new_text = $pw_tag.md5($_POST['submit_pw']).file_get_contents($path);
-				file_put_contents($path, $new_text);
-				}
-		echo '<div class="alert alert-success" role="alert"><strong>设定完成，将在3s后跳转</strong>请务必牢记您的密码！遗失后不可找回！</div><meta http-equiv="refresh" content="3; url=/'.$_GET[f].'">';
-		$isLocked=1;
-		$isEncrypted=1;
-		$needBtn=0;
+	if (dataEncrypt($_POST["submit_pw"]) == $filePw ){
+		//$isLocked=0;
+		$do=0;
+		$_SESSION[dataEncrypt($name)]=1;
 	}else{
-		if (md5($_POST["submit_pw_old"]) == substr($file_head,15,32) ){
-			if (substr($file_head,0,15) == $pw_tag ){
-			$new_text = $pw_tag.md5($_POST['submit_pw']).file_get_contents($path,FALSE,NULL,47);
+		echo loadTips("inputError");
+	}
+}
+
+
+
+if (isset($_POST["setpw"])) {//用户设定密码页面
+	$do=1;
+	if(!$isEncrypted){
+		echo showForms("inputSetPw");
+	}else{
+		echo showForms("inputSetPw_O");
+	}
+}
+
+
+
+if (isset($_POST["setpw2"])) {//后台处理用户设定密码的操作
+	$do=1;
+	if(!$isEncrypted){
+		if($_POST["submit_pw"]){
+			$new_text = $pwTag.dataEncrypt($_POST['submit_pw'])."0".file_get_contents($path);
 			file_put_contents($path, $new_text);
-			}else {
-				$new_text = $pw_tag.md5($_POST['submit_pw']).file_get_contents($path);
-				
-				//echo ($new_text);
-				file_put_contents($path, $new_text);
-				}
-			echo '<div class="alert alert-success" role="alert"><strong>设定完成，将在3s后跳转</strong>请务必牢记您的密码！遗失后不可找回！</div><meta http-equiv="refresh" content="3; url=/'.$_GET[f].'">';
-			$isLocked=1;
+			echo loadTips("setPwSucceed");
+			//$isLocked=1;
 			$isEncrypted=1;
-			$needBtn=0;
+			$_SESSION[dataEncrypt($name)]=0;
 		}else{
-			echo '<div class="alert alert-danger" role="alert"><strong>原密码错误，请在3s后重新认证</strong></div><meta http-equiv="refresh" content="3; url=/'.$_GET[f].'">';
-			$isLocked=1;
+			echo loadTips("setPwNewError");
+		}
+	}else{
+		if (dataEncrypt($_POST["submit_pw_old"]) == $filePw ){
+				if($_POST["submit_pw"]){
+					$new_text = $pwTag.dataEncrypt($_POST['submit_pw']).$fileShare.file_get_contents($path,FALSE,NULL,$fileHeadLen);
+					file_put_contents($path, $new_text);
+					echo loadTips("setPwSucceed");
+					//$isLocked=1;
+					$isEncrypted=1;
+					$_SESSION[dataEncrypt($name)]=0;
+				}else{
+					$new_text = file_get_contents($path,FALSE,NULL,$fileHeadLen);
+					file_put_contents($path, $new_text);
+					echo loadTips("setPwClear");
+					//$isLocked=0;
+					$isEncrypted=0;
+				}
+		}else{
+			echo loadTips("setPwOldError");
+			$_SESSION[dataEncrypt($name)]=0;
+			//$isLocked=1;
 			$isEncrypted=1;
-			$needBtn=0;
 		}
 	}
 }
-if (isset($_POST["t"])) {
-	// Update content of file
-	
-	if (substr($file_head,0,15) == $pw_tag ){
-		
-		$tt = $pw_tag.substr($file_head,15,32).$_POST["t"];
-		
+
+
+
+if (isset($_POST["t"]) and $_SESSION[dataEncrypt($name)]) {//异步处理-笔记内容更新操作
+	if ($isEncrypted){
+		$tt = $pwTag.$filePw.$fileShare.$_POST["t"];
 	}else{
 		$tt = $_POST["t"];
-		}
+	}
 	file_put_contents($path, $tt);
 	die();
 }
-			if(!$do){if (file_exists($path)) {
-				$needBtn=1;
-				if ($isEncrypted){
-					if (!$isLocked) {
-						echo "<textarea class='form-control content'>". htmlspecialchars(file_get_contents($path,FALSE,NULL,47))."</textarea>";
-						$needBtn=1;
-					}else {
-						echo "
-					<br><br>
-					<br><br><br><br><br>
-					<form action='/$_GET[f]' method='post' class='form-inline obody'>
-						<div class='form-group'>
-						<p class='form-control-static'>这是私人笔记，请输入密码：</p>
-						<input type='password' class='form-control' name='submit_pw' /></div>
-						<input type='submit' class='btn btn-default' value='确认' />
-						<input type='hidden' name='checkpw' value='yes' />
-						</form>
-						";
-						$needBtn=0;
-					}
-				}
-				else {
-					echo "<textarea class='form-control content'>". htmlspecialchars(file_get_contents($path))."</textarea>";
-					$needBtn=1;
-				}
-			}
-			else{
-			echo "<textarea class='form-control content'></textarea>";
-					$needBtn=1;
-			}
+
+
+
+if ($_POST["share"]=="1" and $_SESSION[dataEncrypt($name)]) {//开放分享权限
+	$do=1;
+	$md5_path = md5($name);
+	$shareLink="/share/".substr($md5_path,3,6).$name;
+	global $shareLink;
+	if ($isEncrypted){
+
+		$tt = $pwTag.$filePw."1".file_get_contents($path,FALSE,NULL,$fileHeadLen);
+		file_put_contents($path, $tt);
+
+	}//else{
+		//$tt = $_POST["t"];
+	//}
+	echo loadTips("shareOpen");
+	die();
+}/*elseif($_POST["share"]=="0" and $_SESSION[dataEncrypt(substr($name,6))]){
+	$do=1;
+	if ($isEncrypted){
+		if($fileShare=="0"){
+			echo loadTips("shareCloseAlready");
+		}else{
+		$tt = $pwTag.$filePw."0".file_get_contents($path,FALSE,NULL,$fileHeadLen);
+		file_put_contents($path, $tt);
+		echo loadTips("shareCloseSucceed");
+		die();
 		}
+	}else{
+		echo loadTips("shareCloseError");
+	}
+}elseif($_POST["share"]=="0" and !$_SESSION[dataEncrypt(substr($name,6))]){
+	$do=1;
+	echo loadTips("shareCloseError");
+}*/
+
+
+
+if(!$do){//读取笔记内容
+	if (file_exists($path)) {
+		if ($isEncrypted){
+			if ($_SESSION[dataEncrypt($name)]) {
+				echo showNotes("encrypted");
+			}else{
+				echo showForms("inputPw");
+				$do=1;
+			}
+		}else{
+			echo showNotes("decoded");
+		}
+	}else{
+		echo showNotes("");
+	}
+}
+
+
+
+include $templateFolder."footer.php";
 ?>
-</div>
-<?php ($needBtn) ? (include "./content/templates/default/footer.php"):(include "./content/templates/default/footer_nobtn.php");?>
+
+<!--div align="center">
+<p>N.94joy.cn是一个最简洁的云笔记本，您可以使用任意的域名后缀来自定义您的笔记地址。<br>
+比如直接访问 <a href="http://n.94joy.cn/zidingyi">http://n.94joy.cn/zidingyi</a> 即可建立一个自定义地址的云笔记，特别适用于跨平台快速传输一些文本内容。</p>
+<p>由于NotePad.cc停止运营，于是一些用户便发起了NotePad.live的公益项目，并由EverTools基金会托管。<br>
+而由于这个平台的极度便利性，不少人当然也包括我一直在使用，然而好景不长，宣称永不关闭的NotePad.live的公益项目已经无法打开。<br>
+我无法忘记那天晚上当我打不开notepad.live这个网址时的难过，于是我便决定独自发起 N.94joy.cn 的云笔记公益项目。<br>
+我无法作出永不关闭这一难以达成的承诺，但只要服务器不宕机、不丢失文件，域名还是由我管理，这个公益项目就会一直存在。<br>
+如果您觉得这个云笔记本好用，请推荐给身边的人了解。</p>
+<p>Copyright&copy;2018 94joy.cn<br>
+版权所有 只为乐趣<br>
+沪ICP备18047966号</p>
+</div-->
