6693fa7af10a2ddd4ff6fbf57f21d40972c0f4a3
2613df <2613df@163.com>
2019-01-23 16:58:51 +0800

    version 2.2

--- content/templates/default/header.php
+++ content/templates/default/header.php
@@ -4,25 +4,24 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title><?php echo $name;if($readOnly){echo "(只读)";} ?> - 94Notepad</title>
+    <title><?php echo $name;($readOnly)?(print "(只读)"):("");?> - 94Notepad</title>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
 	<style>
 	.content {
-			font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
-			font-size:1.2em;
-			line-height: 1.5em;
-			color: #777;
-			width: 100%;
-			height: 100%;
-			min-height: 100%;
-			resize: none;
-			overflow-y: auto;
-			-webkit-box-sizing: border-box;
-			-moz-box-sizing: border-box;
-			box-sizing: border-box;
-		}
-		textarea {
+		font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
+		font-size:1.2em;
+		line-height: 1.5em;
+		color: #777;
+		width: 100%;
+		height: 100%;
+		min-height: 100%;
+		resize: none;
+		overflow-y: auto;
+		-webkit-box-sizing: border-box;
+		-moz-box-sizing: border-box;
+		box-sizing: border-box;
+	}
+	textarea {
 		overflow: auto;
 		vertical-align: top;
 	}
@@ -34,27 +33,23 @@
 		right: 0;
 		bottom: 5%;
 	}
-
-	pre{
-white-space: pre-wrap;       
-white-space: -moz-pre-wrap;  
-white-space: -pre-wrap;      
-white-space: -o-pre-wrap;    
-word-wrap: break-word;       
-}
-body {padding-top: 5em;}
-.txa{
-	position:absolute;
-	top:10%;
-	bottom:10%;
-	left:5%;
-	right:5%;
-	
+	pre {
+		white-space: pre-wrap;       
+		white-space: -moz-pre-wrap;  
+		white-space: -pre-wrap;      
+		white-space: -o-pre-wrap;    
+		word-wrap: break-word;       
 	}
-.obody{
-text-align:center;
-}
-*{
+	body {padding-top: 5em;}
+	.txa {
+		position:absolute;
+		top:10%;
+		bottom:10%;
+		left:5%;
+		right:5%;
+	}
+	.obody {text-align:center;}
+	* {
 		transition: all 0.5s;
 		-moz-transition: all 0.5s;
 		-webkit-transition: all 0.5s;
